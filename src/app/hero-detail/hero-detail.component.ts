17283e9e3a36040339f6c3809b98a8af2268e983
Savannah Pyle <savannah.pylee@gmail.com>
2019-11-22 13:52:30 -0700

    enabled http services

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { Hero } from "../hero";
 import { ActivatedRoute } from "@angular/router";
 import { Location } from "@angular/common";
+
+import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
 
 @Component({
@@ -29,5 +30,9 @@ export class HeroDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 }
